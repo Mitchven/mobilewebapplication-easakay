2d181a7bcdeb6de19c7e9ad41eb9504e56e23f88
Lucifer Morningstar <star808fer@gmail.com>
2019-11-14 20:48:15 +0800

    kapoy nako

--- src/components/frame/Header.vue
+++ src/components/frame/Header.vue
@@ -1,39 +1,41 @@
 <template>
   <div fixed="top">
-    <b-card  no-body>
-      <b-card-header  header-tag="nav" class="bg-success">
-        <b-nav  card-header tabs>
-          <b-nav-item  active v-if="auth.user != null">Easakay</b-nav-item>
-          <b-nav-item @click="redirect('/Login')"  v-if="auth.user ===null"  id="login">Sign In</b-nav-item>
-          <b-nav-item @click="redirect('/Register')"  v-if="auth.user ===null"  id="signup">Sign Up</b-nav-item>
+    <b-card no-body>
+      <b-card-header header-tag="nav" class="bg-success">
+        <b-nav class="card-header tabs" >
+          <b-nav-item active v-if="auth.user != null">Easakay</b-nav-item>
+
+          <b-nav @click="redirect('/Login')" v-if="auth.user ===null" id="login">Sign In</b-nav>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <b-nav @click="redirect('/Register')" v-if="auth.user ===null" id="signup">Sign Up</b-nav>
         </b-nav>
       </b-card-header>
-
-     
     </b-card>
   </div>
 </template>
-<style scoped lang="scss">
-@import "assets/style.scss";
-#login{
-  color:white;
+<style scoped >
+/* // @import "assets/style.scss"; */
+#login {
+  color: white;
   font-size: 20px;
 }
-#signup{
-  color:white;
+.card-header tabs {
+  padding-bottom: 50%;
+   padding-top: 50%;
+}
+#signup {
+  color: white;
   font-size: 20px;
 }
-
-
 </style>
 <script>
-import AUTH from 'services/auth'
+import AUTH from "services/auth";
 import ROUTER from "router";
 export default {
-  data(){
+  data() {
     return {
       auth: AUTH
-    }
+    };
   },
   methods: {
     redirect(router) {
--- src/modules/LandingPage.vue
+++ src/modules/LandingPage.vue
@@ -1,28 +1,32 @@
 <template>
-    <div class="circular" v-bind:style="{ 'background-image': 'url(  )' }">
-        <!-- <img src = ""> -->
+  <div id="card">
+    <b-card-body>
+      <div class="circular" v-bind:style="{ 'background-image': 'url(  )' }">
         <p id="p">"Value everyone's comfort in the world of travel"</p>
         <button id="btn">Learn more</button>
-    </div>
+      </div>
+    </b-card-body>
+  </div>
 </template>
 <style>
 #p {
-    font-size:300%;
-    margin-top:10%;
-    margin-left: 20%;
+  font-size: 80px;
+  margin-top: 10%;
+  margin-left: 8%;
+  text-align: center;
 }
 #btn {
-    background-color: green;
-    margin-left: 45%;
-    color:white;
-    padding-left: 2%;
-    padding-right: 2%;
-    padding-top:1%;
-    padding-bottom: 1%;
+  background-color: green;
+  margin-left: 45%;
+  color: white;
+  padding-left: 2%;
+  padding-right: 2%;
+  padding-top: 1%;
+  padding-bottom: 1%;
 }
 div {
-    /* background:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvn1vhmcUVSTg3nxpYw6rfKXaApuXrjSnv4t62ozKeet_xUaceoQ&s);*/
-    background-repeat: no-repeat;
-    position:cover;
+  /* background:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvn1vhmcUVSTg3nxpYw6rfKXaApuXrjSnv4t62ozKeet_xUaceoQ&s);*/
+  background-repeat: no-repeat;
+  position: cover;
 }
 </style>
